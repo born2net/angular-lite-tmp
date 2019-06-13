25fd27ffed42eeb8544931fd3b6e1d75150eb924
born2net@gmail.com <connectconnect123>
2019-06-13 12:35:30 -0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,30 @@
-import { Component } from '@angular/core';
-import { Router, NavigationEnd, ActivatedRoute, Data } from '@angular/router';
+import {Component} from '@angular/core';
+import {Router, NavigationEnd, ActivatedRoute, Data} from '@angular/router';
 
-import { environment } from 'environments/environment';
+import {environment} from 'environments/environment';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
-  name = environment.appName;
-  title: string;
+    name = environment.appName;
+    title: string;
 
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute
-  ) {
-    router.events
-      .filter((event) => event instanceof NavigationEnd)
-      .map(() => this.route.firstChild)
-      .filter((childRoute) => childRoute !== null)
-      .switchMap((childRoute) => childRoute.data)
-      .filter((data) => !!data)
-      .subscribe((data: Data) => {
-        this.title = data.title;
-      });
-  }
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute
+    ) {
+        router.events
+            .filter((event) => event instanceof NavigationEnd)
+            .map(() => this.route.firstChild)
+            .filter((childRoute) => childRoute !== null)
+            .switchMap((childRoute) => childRoute.data)
+            .filter((data) => !!data)
+            .subscribe((data: Data) => {
+                this.title = data.title;
+            });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,81 +1,82 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
 import {
-  AuthGuard,
-  AuthInterceptor,
-  CalculationService,
-  CategoryService,
-  DeactivateGuard,
-  ErrorResponseInterceptor,
-  LoginService,
-  PostService,
-  ReversalAuthGuard,
-  StyleService,
-  UserService
-} from './services';
-import { AuthStore, CalculationStore, CategoryStore, PostStore, UserStore } from './stores';
-
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-
-import { HOME_URL, LOGIN_URL, TIME_RANGE_SEPARATOR } from 'app/common';
-import { MainModule } from './layouts';
-import { LoginModule, NotFoundModule } from './containers';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserAnimationsModule,
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    MainModule,
-    LoginModule,
-    NotFoundModule
-  ],
-  providers: [
     AuthGuard,
+    AuthInterceptor,
     CalculationService,
     CategoryService,
     DeactivateGuard,
+    ErrorResponseInterceptor,
     LoginService,
     PostService,
     ReversalAuthGuard,
     StyleService,
-    UserService,
-    AuthStore,
-    CalculationStore,
-    CategoryStore,
-    PostStore,
-    UserStore,
-    {
-      provide: HOME_URL,
-      useValue: ''
-    },
-    {
-      provide: LOGIN_URL,
-      useValue: 'login'
-    },
-    {
-      provide: TIME_RANGE_SEPARATOR,
-      useValue: ' - '
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: ErrorResponseInterceptor,
-      multi: true
-    }
-  ],
-  bootstrap: [AppComponent]
+    UserService
+} from './services';
+import {AuthStore, CalculationStore, CategoryStore, PostStore, UserStore} from './stores';
+
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+
+import {HOME_URL, LOGIN_URL, TIME_RANGE_SEPARATOR} from 'app/common';
+import {MainModule} from './layouts';
+import {LoginModule, NotFoundModule} from './containers';
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserAnimationsModule,
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        MainModule,
+        LoginModule,
+        NotFoundModule
+    ],
+    providers: [
+        AuthGuard,
+        CalculationService,
+        CategoryService,
+        DeactivateGuard,
+        LoginService,
+        PostService,
+        ReversalAuthGuard,
+        StyleService,
+        UserService,
+        AuthStore,
+        CalculationStore,
+        CategoryStore,
+        PostStore,
+        UserStore,
+        {
+            provide: HOME_URL,
+            useValue: ''
+        },
+        {
+            provide: LOGIN_URL,
+            useValue: 'login'
+        },
+        {
+            provide: TIME_RANGE_SEPARATOR,
+            useValue: ' - '
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptor,
+            multi: true
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ErrorResponseInterceptor,
+            multi: true
+        }
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
